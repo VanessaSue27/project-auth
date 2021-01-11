4ed9ea799edd659aad5ff89ed2a3b7e21170827e
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2021-01-11 16:07:11 +0100

    Add package to show list of endpoints in '/' path

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import crypto from 'crypto';
 import bcrypt from 'bcrypt';
+import listEndpoints from 'express-list-endpoints';
 
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/authAPI';
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -57,6 +58,10 @@ const authenticateUser = async (req, res, next) => {
 };
 
 // ROUTES
+// Landing Page: will show a list of the existing endpoints
+app.get('/', (req, res) => {
+  res.send(listEndpoints(app));
+});
 
 // REGISTRATION ENDPOINT - to create a NEW account - Sign Up
 // This endpoint expects a name and password in the body from the POST request from the Frontend
